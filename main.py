5c6b6fa2a7c50146601a97512ef9319c1a031fed
Aaryan Regmi <53842807+Aaryan73@users.noreply.github.com>
2020-09-11 17:26:47 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-def intlistelement(numlist):
+def intlistelement(numlist):   # convert the items of list into integer
   for j in range(len(numlist)):
     numlist[j] = int(numlist[j])
   return numlist
 
-def rotate(intlist, k):
+def rotate(intlist, k):    # bring the element from last of the list to first
     lastelement = 0
-    for i in range(k):    
+    for i in range(k):      # for rotating the elements k number of times 
         lastelement = intlist[len(intlist)-1]
         intlist.pop()
         intlist.insert(0, lastelement)
     return intlist
   
-def output(intlist, n):
+def output(intlist, n):   # formats the list for output
   stroutput = ""
   for o in intlist:
     stroutput += str(o)
@@ -20,12 +20,12 @@ def output(intlist, n):
 
 t = int(input())
 i = 1
-while i <= t:
+while i <= t:                          # 
     n, k = input().split(" ")
     n, k = int(n), int(k)
     numlist = input().split(" ")
-    intlist = intlistelement(numlist)
-    intlist = rotate(intlist, k)    
-    intlist = output(intlist, n)
+    intlist = intlistelement(numlist)     # list items gets converted into integer
+    intlist = rotate(intlist, k)      # brings last elements to first
+    intlist = output(intlist, n)     # formats the items of intlist for output
     print(intlist)
     i += 1
